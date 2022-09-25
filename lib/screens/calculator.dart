43760a919d5c67769e9c74dae357d5dfd754f5e8
Jerah Borges <jerah_borges@hotmail.com>
2022-09-25 02:08:10 -0300

    Verção 1.1

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../components/display.dart';
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Column(children: <Widget>[
+      Display('123.45'),
+      Text('Keyboard'),
+    ]));
+  }
+}
